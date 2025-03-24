f021db19d45ccdbebe7eea7737871f4b9f207746
Cassius Tedesco <cassiustedesco1@gmail.com>
2025-03-24 09:22:30 +0100

    first commit

---
+++ controller/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+class UserController{
+    
+    public function register() : void {
+        
+    }
+    public function login() : void {
+        
+    }
+    public function logout() : void {
+        
+    }
+}
