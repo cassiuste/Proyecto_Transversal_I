87d697145b12aaf4b0dd361982ef3f8eb870b84b
Jordi <jlf20061@gmail.com>
2025-03-25 10:07:47 +0100

    changes commit

--- view/auth.php
+++ view/auth.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EventLink</title>
+    <link rel="stylesheet" href="auth.css">
 </head>
 <body>
     <form action="" id="registerForm">
         <h1>Register</h1>
+        <input id="nameSignIn"></input>
+        <input id="emailSignIn"></input>
+        <input id="passwordSignIn"></input>
     </form>
     
 </body>
