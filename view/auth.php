ddce5cc50985eae8902b2f0771ce7adef135ff14
Jordi <jlf20061@gmail.com>
2025-03-27 10:19:56 +0100

    created auth

--- view/auth.php
+++ view/auth.php
@@ -7,12 +7,25 @@
     <link rel="stylesheet" href="auth.css">
 </head>
 <body>
-    <form action="" id="registerForm">
-        <h1>Register</h1>
-        <input id="nameSignIn"></input>
-        <input id="emailSignIn"></input>
-        <input id="passwordSignIn"></input>
+
+    <div id="authDiv">
+    <h1 id="title">EventLink</h1>
+    <form action="" id="signUpForm">
+        <h1 id="signUpTitle">Sign Up</h1>
+        <input id="nameSignUp" placeholder="Name"></input><br>
+        <input id="emailSignUp" placeholder="Email"></input><br>
+        <input id="passwordSignUp"placeholder="Password"></input>
+        <p>Already have an account? <a id="goToSignIn">Sign In</a></p>
     </form>
+
+    <form action="" id="signInForm">
+        <h1 id="signInTitle">Sign In</h1>
+        <input id="emailSignIn" placeholder="Email"></input><br>
+        <input id="passwordSignIn" placeholder="Password"></input>
+        <p>Don't have an account? <a id="goToSignUp">Sign Up</a></p>
+    </form>
+    </div>
     
+    <script src="auth.js"></script>
 </body>
 </html>
