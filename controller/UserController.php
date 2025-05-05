95c44d66c26d241b9e645aec945c297bae84cede
Cassius Tedesco <cassiustedesco1@gmail.com>
2025-05-05 10:21:21 +0200

    changed image controll

--- controller/UserController.php
+++ controller/UserController.php
@@ -86,8 +86,8 @@ class UserController{
                        $file_ext = pathinfo($file_name, PATHINFO_EXTENSION);
                        $allowed_ext = array('jpg', 'jpeg', 'png', 'gif');   
                        $subfolder = '../view/img/profile/admin/';
-                       /* $new_file_name = uniqid() . '.' . $file_ext; */
-                       $destination = $subfolder . $file_ext;
+                       $new_file_name = uniqid() . '.' . $file_ext;
+                       $destination = $subfolder . $new_file_name;
                        
                        if (in_array($file_ext, $allowed_ext)) {
                         move_uploaded_file($file_tmp, $destination);
