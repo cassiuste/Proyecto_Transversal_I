8dcf33fc3cb8deddf3a1907a348b7434b78c8a2e
patricia <patricia.gsbcn@gmail.com>
2025-05-16 11:27:20 +0200

    Elimino mensaje conexion existosa BD

--- controller/EventController.php
+++ controller/EventController.php
@@ -39,7 +39,6 @@ class EventController{
             $this->conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
             // set the PDO error mode to exception
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Connected successfully";
           } catch(PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
           }
